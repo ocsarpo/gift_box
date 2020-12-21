e2553d6582a6be81699d851f23df509022a85026
ocsarpo <ocsarpo22@gmail.com>
2020-12-21 23:04:31 +0900

    draw

--- app/services/draw_service.rb
+++ app/services/draw_service.rb
@@ -5,31 +5,29 @@ class DrawService
 
   def random
     numbers = [*1..45]
-    wins = []
-    
-    1.upto(6) do
-      win = numbers.sample
-      wins << win
-      numbers.reject!{|n| n == win}
+
+    wins = (1..6).to_a.inject([]) do |_wins, _|
+      num = numbers.shuffle.sample
+      numbers.reject! {|_num| _num == num}
+      _wins << num
     end
-    wins.sort!
+    
+    wins.sort
   end
 
   def draw
     numbers = LottoNumber.all
     total = numbers.sum(&:draw_probability)
-    wins = []
     
+    wins = Set.new
+
     while wins.length < 6 do
+      num = numbers.shuffle.sample
       draw_num = rand(total) + 1
-      numbers.each do |num|
-        draw_num -= num.draw_probability
-        if draw_num <= 0
-          wins << num.num unless wins.include? num.num
-          break
-        end
-      end
+      draw_num -= num.draw_probability
+      wins.add(num.num) if draw_num <= 0
     end
-    wins.sort!
+
+    wins.to_a.sort
   end
 end
